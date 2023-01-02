a8061e771b3328e8c028ac4ee72745e60d6844bc
Riya-Tiwari03 <93140944+Riya-Tiwari03@users.noreply.github.com>
2023-01-02 17:05:17 +0530

    Update InvalidMailIDException.java

--- LabTest4/LabTest4/src/Exceptions/InvalidMailIDException.java
+++ LabTest4/LabTest4/src/Exceptions/InvalidMailIDException.java
@@ -1,5 +1,23 @@
 package Exceptions;
 
 public class InvalidMailIDException {
+public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			// declare the email variable
+			String Email;
+			// taking the user input
+			System.out.print("Enter an Email : ");
+			Email = sc.nextLine();
+			try {
+				if(Pattern.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",Email )) {
+					System.out.println("Email is Correct : "+Email);
+				}else {
+					throw new MailIdException("Email is not Correct : "+Email);
+				}
+			}catch(MailIdException e) {
+				System.out.println(e.getMessage());
+			}
+			
 
+		}
 }
